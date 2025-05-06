4d8b01535c46acd6da59e7ca691bf80cfb479fa7
Enrique Mahardhika <enrique.22031@mhs.unesa.ac.id>
2025-05-06 10:47:34 +0700

    add pop up form

--- noslider/application/controllers/Mobile.php
+++ noslider/application/controllers/Mobile.php
@@ -5,7 +5,7 @@ class Mobile extends CI_Controller {
 
 	public function index()
 	{
-        $this->load->view('dokter_bermobile');
+        // $this->load->view('header');
         $this->load->view('jammapsmobile');
 	}
 }
