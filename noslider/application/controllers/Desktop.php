6fc5344ba85a565eb4216006226991f080655625
Enrique Mahardhika <enrique.22031@mhs.unesa.ac.id>
2025-05-08 14:11:15 +0700

    fixing mobile

--- noslider/application/controllers/Desktop.php
+++ noslider/application/controllers/Desktop.php
@@ -7,8 +7,12 @@ class Desktop extends CI_Controller {
 	{
 		// $this->load->view('header');
 		// $this->load->view('service');
+		// $this->load->view('dokter_berita');
+		// $this->load->view('jammaps');
+		// $this->load->view('footer');
+		$this->load->view('headermobile');
+		$this->load->view('servicemobile');
 		$this->load->view('dokter_bermobile');
 		$this->load->view('jammapsmobile');
-		$this->load->view('footer');
 	}
 }
--- noslider/application/views/servicemobile.php
+++ noslider/application/views/servicemobile.php
@@ -6,19 +6,17 @@
     <title>Klinik Mata dr. Sjamsu</title>
     <style>
         
-        
         .container-service {
             max-width: 1200px;
-            margin: 0 auto;
+            /* margin: 0 auto; */
             /* padding: 20px; */
         }
 
         .background-service {
             background-color: rgba(0, 170, 180, 0.05);
             padding: 40px 20px;
-            margin-top: 40px;
-    }
-
+            /* margin-top: 40px; */
+        }
         
         .header-service {
             text-align: center;
@@ -42,6 +40,7 @@
             flex-direction: column;
             align-items: flex-start;
             text-align: left;
+            margin-bottom: 15px;
         }
 
         .service-card:hover,
@@ -87,70 +86,117 @@
             color: #838383;
         }
         
-        
+        /* Media Query untuk tampilan mobile */
+        @media screen and (max-width: 768px) {
+            .services-grid {
+                grid-template-columns: 1fr; /* Mengubah menjadi 1 kolom pada mobile */
+                gap: 15px;
+            }
+            
+            .service-card {
+                padding: 20px;
+            }
+            
+            .service-title {
+                font-size: 18px;
+            }
+            
+            .icon-circle {
+                width: 50px;
+                height: 50px;
+            }
+            
+            .background-service {
+                padding: 20px 15px;
+            }
+            
+            .header-service h1 {
+                font-size: 24px;
+            }
+        }
     </style>
 </head>
 <body>
-    <div class="background-service">
     <div class="container-service">
+    <div class="background-service">
         <div class="header-service">
             <h1>Klinik Mata dr. Sjamsu</h1>
         </div>
         
         <div class="services-grid">
-            <!-- Card 1 - Purple Border -->
-            <div class="service-card purple-border">
+            <!-- Card 1 -->
+            <div class="service-card">
                 <div class="icon-circle">
                     <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                     </svg>
-
-
                 </div>
-                <h3 class="service-title">Skrining dan Check Up</h3>
+                <h3 class="service-title">Skrining & Check Up</h3>
                 <p class="service-desc">
                     Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
                 </p>
             </div>
             
-            <!-- Card 2 - White Background -->
+            <!-- Card 2 -->
             <div class="service-card">
                 <div class="icon-circle">
                 <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                     </svg>
-
-
                 </div>
-                <h3 class="service-title">Skrining dan Check Up</h3>
+                <h3 class="service-title">Skrining & Check Up</h3>
                 <p class="service-desc">
                     Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
                 </p>
             </div>
             
-            <!-- Card 3 - Teal Background -->
+            <!-- Card 3 -->
             <div class="service-card">
                 <div class="icon-circle">
                 <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                     </svg>
-
                 </div>
-                <h3 class="service-title">Skrining dan Check Up</h3>
+                <h3 class="service-title">Skrining & Check Up</h3>
                 <p class="service-desc">
                     Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
                 </p>
             </div>
             
-            <!-- Card 4 - White Background -->
+            <!-- Card 4 -->
             <div class="service-card">
                 <div class="icon-circle">
                 <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                     </svg>
-
                 </div>
-                <h3 class="service-title">Skrining dan Check Up</h3>
+                <h3 class="service-title">Skrining & Check Up</h3>
+                <p class="service-desc">
+                    Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
+                </p>
+            </div>
+            
+            <!-- Card 5 (tambahan sesuai gambar) -->
+            <div class="service-card">
+                <div class="icon-circle">
+                <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                    </svg>
+                </div>
+                <h3 class="service-title">Skrining & Check Up</h3>
+                <p class="service-desc">
+                    Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
+                </p>
+            </div>
+            
+            <!-- Card 6 (tambahan sesuai gambar) -->
+            <div class="service-card">
+                <div class="icon-circle">
+                <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00CDDA" width="50px">
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                    </svg>
+                </div>
+                <h3 class="service-title">Skrining & Check Up</h3>
                 <p class="service-desc">
                     Kami turut berkontribusi dalam pencegahan gangguan kesehatan mata sejak dini yang memberikan pelayanan berupa skrining dan check up secara berkala
                 </p>
