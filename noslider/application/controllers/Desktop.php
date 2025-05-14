4f640096b9516852b6858d630a10d026b599fc48
ainul <ainulmufidh@gmail.com>
2025-05-14 11:13:51 +0700

    revisi header & hero

--- noslider/application/controllers/Desktop.php
+++ noslider/application/controllers/Desktop.php
@@ -6,10 +6,17 @@ class Desktop extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('Desktop/header');
+		$this->load->view('Desktop/hero');
 		$this->load->view('Desktop/service');
 		$this->load->view('Desktop/popup');
 		$this->load->view('Desktop/dokter_berita');
 		$this->load->view('Desktop/jammaps');
     	$this->load->view('Desktop/footer');
+	}
+
+	public function Doctor()
+	{
+		$this->load->view('Desktop/header');
+		$this->load->view('Desktop/dokter_page');
 	}
 }
