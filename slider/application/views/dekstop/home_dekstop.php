9223be672a8dc62d8726b6cc464a9230770765e7
MiftakhFaizin <miftakhdamar07@gmail.com>
2025-06-05 19:03:31 +0700

    fix navbar

--- slider/application/views/dekstop/home_dekstop.php
+++ slider/application/views/dekstop/home_dekstop.php
@@ -623,10 +623,10 @@
                 </div>
                 <div id="menu-navbar" class="d-flex flex-row align-items-center">
                     <a href="<?= base_url('Dekstop') ?>" style="text-decoration: none; color: black;"><p class="menu-border">Beranda</p></a>
-                    <p>Pelayanan</p>
+                    <a href="<?= base_url('Dekstop/layanan') ?>" style="text-decoration: none; color: black;"><p class="menu-border">Pelayanan</p></a>
                     <a href="<?= base_url('Dekstop/dokter_page') ?>" style="text-decoration: none; color: black;"><p>Dokter</p></a>
                     <a href="<?= base_url('Dekstop/jadwal_dokter') ?>" style="text-decoration: none; color: black;"><p class="menu-border">Jadwal Dokter</p></a>
-                    <p class="menu-border">Berita Terbaru</p>
+                    <a href="<?= base_url('News') ?>" style="text-decoration: none; color: black;"><p class="menu-border">Berita</p></a>
                 </div>
             </section>
 
